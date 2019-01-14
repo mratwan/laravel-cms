93a52fcb1030b0f37c09f877dbeff62d8d354e3d
Mohammadreza Atwan <mratwan@gmail.com>
2019-01-14 15:09:00 +0330

    update sidebar links

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -104,12 +104,17 @@
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{ route('logout') }}" class="nav-link" 
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                             <i class="nav-icon fas fa-power-off"></i>
                             <p>
-                                Logout
+                                {{ __('Logout') }}
                             </p>
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                     
                 </ul>
