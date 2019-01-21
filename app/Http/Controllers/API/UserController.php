9c9577101a0b6eb0433f4bcd6d59619c04164ffd
Mohammadreza Atwan <mratwan@gmail.com>
2019-01-21 23:32:07 +0330

    update profile options and api in panel

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -61,6 +61,17 @@ class UserController extends Controller
     }
 
     /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function profile($id)
+    {
+        return auth('api')->user();
+    }
+
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request $request
--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,12 +1,98 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Profile Component</div>
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <div class="card card-widget widget-user" style="padding: 0">
+                    <div class="widget-user-header text-white" style="background: url('/images/bg1.png') center center;background-size: cover;height: 250px;">
+                        <h3 class="widget-user-username">Mohammadreza Atwan</h3>
+                        <h5 class="widget-user-desc">Web Designer</h5>
+                    </div>
+                    <div class="widget-user-image">
+                        <img class="img-circle" src="/images/mratwan.jpg" alt="User Avatar">
+                    </div>
+                    <div class="card-footer" style="padding: 0">
+                        <div class="row">
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">3,200</h5>
+                                    <span class="description-text">SALES</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">13,000</h5>
+                                    <span class="description-text">FOLLOWERS</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="description-block">
+                                    <h5 class="description-header">35</h5>
+                                    <span class="description-text">PRODUCTS</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header p-2">
+                        <ul class="nav nav-pills">
+                            <li class="nav-item"><a class="nav-link" href="#activity" data-toggle="tab">Activity</a></li>
+                            <li class="nav-item"><a class="nav-link active show" href="#settings" data-toggle="tab">Settings</a></li>
+                        </ul>
+                    </div>
                     <div class="card-body">
-                        I'm an example component.
+                        <div class="tab-content">
+                            <div class="tab-pane" id="activity">
+                                <h3 class="text-center">Display User Activity</h3>
+                            </div>
+                            <div class="tab-pane active show" id="settings">
+                                <form class="form-horizontal">
+                                    <div class="form-group">
+                                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+                                        <div class="col-sm-12">
+                                            <input type="text" v-model="form.name" class="form-control" id="inputName" placeholder="Name">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                                        <div class="col-sm-12">
+                                            <input type="email" v-model="form.email" class="form-control" id="inputEmail" placeholder="Email">
+                                            <has-error field="email"></has-error>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+                                        <div class="col-sm-12">
+                                            <textarea class="form-control" id="inputExperience" placeholder="Experience" :class=""></textarea>
+                                            <has-error field="bio"></has-error>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo" class="col-sm-2 control-label">Profile Photo</label>
+                                        <div class="col-sm-12">
+                                            <input id="photo" type="file" @change="" name="photo" class="form-input">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password" class="col-sm-12 control-label">Passport (leave empty if not changing)</label>
+                                        <div class="col-sm-12">
+                                            <input type="password" class="form-control" id="password" placeholder="Passport">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-2 col-sm-12">
+                                            <button type="submit" class="btn btn-success">Update</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -16,8 +102,24 @@
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        created() {
+            axios.get('api/profile').then(({ data }) => ( this.form.fill(data) ) )
         }
     }
 </script>
